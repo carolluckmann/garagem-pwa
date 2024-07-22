ca5846a6dcf902b4197f246eb7c30128bc5e1a88
carolluckmann <carolinaluckmann05@gmail.com>
2024-07-22 11:56:53 -0300

    garagem-pwa

---
+++ src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import "@passageidentity/passage-elements/passage-auth";
+</script>
+
+<template>
+  <h1>Login</h1>
+  <div class="authContainer">
+    <passage-auth app-id="CFK8bkfMglv51Njjz9EETfka"></passage-auth>
+  </div>
+</template>
+
+<style></style>
